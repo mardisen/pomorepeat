13ecd44249520a357f0133f6f05f9e90a381e8d5
mardisen <marino.didio.work@gmail.com>
2022-05-22 10:54:05 +0200

    Implemented alias for primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     './index.html',
@@ -6,6 +8,9 @@ module.exports = {
   darkMode: 'class',
   theme: {
     extend: {},
+    colors: {
+      primary: colors.green
+    }
   },
   plugins: [],
 };
